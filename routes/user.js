6a5652eeaf4355a168bca75e44a14641394376a7
Mike Wilcox <mwilcox56@gmail.com>
2014-08-18 00:41:05 +0000

    .

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
   'use strict';
-  var User = require('../db/models/user')();
+  var User = require('../db/models/user');
 
   app.get('/user/:id', function(req, res) {
     res.send('got ye user! ar! looks like ye searchin\' fer ' + req.params.id);
