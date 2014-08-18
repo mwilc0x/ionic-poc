07ed19e48dee4f7fa21270cd889aa4ff02de050c
Mike Wilcox <mwilcox56@gmail.com>
2014-08-18 00:04:46 +0000

    .

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
   'use strict';
-  app.get('/user', function(req, res) {
-    res.send('got ye user! ar!');
+  app.get('/user/:id', function(req, res) {
+    res.send('got ye user! ar! lookin like ye searchin\' fer ' + req.params.id);
   });
 };
