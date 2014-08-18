87fea1a60164b58f0bef0c8a3950ec1402d9daff
Mike Wilcox <mwilcox56@gmail.com>
2014-08-17 20:04:12 -0400

    experiment

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
   'use strict';
-  app.get('/user/:id', function(req, res) {
+  app.get('/register/:name', function(req, res) {
     res.send('got ye user! ar! looks like ye searchin\' fer ' + req.params.id);
   });
 };
