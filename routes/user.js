618fa0cca3ad827d8b94d1bf989924fcdbb55603
Mike Wilcox <mwilcox56@gmail.com>
2014-08-17 20:51:13 -0400

    .

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ module.exports = function (app) {
   app.get('/user/:id', function(req, res) {
     res.send('got ye user! ar! looks like ye searchin\' fer ' + req.params.id);
     var user = new User({ firstName: req.params.id });
+    console.log('User ' + user );
 
     // MongoDB will create the _id when inserted
     user.save(function (err) {
