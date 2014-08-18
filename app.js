bfad91db844390748dbf28825fcfc603c4feb71d
Mike Wilcox <mwilcox56@gmail.com>
2014-08-17 20:39:01 -0400

    changes

--- app.js
+++ app.js
@@ -6,14 +6,11 @@
       express = require('express'),
       app = express(),
       server = http.createServer(app),
-      //io = require('socket.io'),
-      //io = io.listen(server),
-      db = require('./db/config')();
+      config = require('./db/config')();
 
-  mongoose.connect(db.path);
+  mongoose.connect(config.path);
   mongoose.connection;
 
-  //require('./sockets/base')(io);
   require('./routes/main')(app);
   require('./routes/user')(app);
 
@@ -25,8 +22,6 @@
     }
   });
 
-  console.log('Starting server');
-  //server.listen(3000);
   app.listen(8080);
-  console.log('Webserver listening on port 8080');
+  console.log('Server listening...');
 })();
