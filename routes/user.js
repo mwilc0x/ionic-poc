eb0be44c005c98931014c94711fbc286b0cab074
Mike Wilcox <mwilcox56@gmail.com>
2014-08-17 20:48:01 -0400

    changes

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,15 @@ module.exports = function (app) {
 
   app.get('/user/:id', function(req, res) {
     res.send('got ye user! ar! looks like ye searchin\' fer ' + req.params.id);
+    var user = new User({ firstName: req.params.id });
+
+    // MongoDB will create the _id when inserted
+    user.save(function (err) {
+      if (err) return handleError(err);
+      User.findById(user, function (err, doc) {
+        if (err) return handleError(err);
+        console.log(doc); // { name: 'mongodb.org', _id: '50341373e894ad16347efe12' }
+      })
+    })
   });
 };
